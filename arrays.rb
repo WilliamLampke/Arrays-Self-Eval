eeb34d9cde48dc49e617859442154dae69809442
William Lampke <gravitybangers@gmail.com>
2022-08-03 12:33:39 -0400

    indexing explained

--- arrays.rb
+++ arrays.rb
@@ -11,3 +11,6 @@ float_array.unshift(1.0)
 ''' unshift(1.0) will add 1.0 at the first index of the array '''
 bool_array.shift
 ''' shift will remove the first true from the array '''
+
+''' indexing always starts at 0 for example 1 in int_array is index 0 you can also start from the end with -1,
+ -1 in int_array is 4, -2 would be 3. You can use indexing to cycle through arrays and other data structures '''  
