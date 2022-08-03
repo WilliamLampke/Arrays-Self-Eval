473a0522c6622d048711e52530bec2f81323223b
William Lampke <gravitybangers@gmail.com>
2022-08-03 12:26:17 -0400

    methods used with comments

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,13 @@
 string_array = ["one", "two", "three", "four"]
 int_array = [1, 2, 3, 4]
 float_array = [1.1, 1.2, 1.3, 1.4]
-book [true, true, false, false]
+bool_array = [true, true, false, false]
+
+string_array.pop
+''' Poping will remove four from the array '''
+int_array.push(5)
+''' push(5) will add 5 at the end of the array '''
+float_array.unshift(1.0)
+''' unshift(1.0) will add 1.0 at the first index of the array '''
+bool_array.shift
+''' shift will remove the first true from the array '''
